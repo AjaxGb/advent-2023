167d6be5adc3172033c386972bf206c376b36746
Sol Toder <ajaxgb@gmail.com>
2023-12-06 00:45:46 -0500

    Day 03 p2

--- src/day03/main.rs
+++ src/day03/main.rs
@@ -10,20 +10,25 @@ struct Num {
 }
 
 impl Num {
-    pub fn check_border<F>(&self, check: F) -> bool
+    pub fn check_border<F>(&self, mut check: F)
     where
-        F: Fn(i32, i32) -> bool,
+        F: FnMut(i32, i32),
     {
         let border_min_x = self.min_x - 1;
         let border_max_x = self.max_x + 1;
         let y = self.y;
         for x in border_min_x..=border_max_x {
-            if check(x, y - 1) || check(x, y + 1) {
-                return true;
-            }
+            check(x, y - 1);
+            check(x, y + 1);
         }
-        check(border_min_x, y) || check(border_max_x, y)
+        check(border_min_x, y);
+        check(border_max_x, y);
     }
+}
+
+struct Part {
+    symbol: char,
+    labels: Vec<u32>,
 }
 
 fn main() {
@@ -49,15 +54,41 @@ fn main() {
                 });
             } else {
                 let x = item.start() as i32;
-                parts.insert((x, y), item.as_str());
+                parts.insert(
+                    (x, y),
+                    Part {
+                        symbol: item.as_str().parse().unwrap(),
+                        labels: Vec::new(),
+                    },
+                );
             }
         }
     }
 
-    let result_p1: u32 = nums
-        .iter()
-        .filter(|n| n.check_border(|x, y| parts.contains_key(&(x, y))))
-        .map(|n| n.value)
-        .sum();
+    let mut result_p1 = 0;
+
+    for num in nums {
+        let mut has_part = false;
+        num.check_border(|x, y| {
+            if let Some(part) = parts.get_mut(&(x, y)) {
+                has_part = true;
+                part.labels.push(num.value);
+            }
+        });
+        if has_part {
+            result_p1 += num.value;
+        }
+    }
+
     println!("Part 1: {result_p1}");
+
+    let mut result_p2 = 0;
+
+    for part in parts.into_values() {
+        if let ('*', [a, b]) = (part.symbol, part.labels.as_slice()) {
+            result_p2 += a * b;
+        }
+    }
+
+    println!("Part 2: {result_p2}");
 }
