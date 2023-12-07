5deb8fe861c933712e2b354fb695d1f183ac0fee
Sol Toder <ajaxgb@gmail.com>
2023-12-06 20:07:39 -0500

    Fix bug in simple_parse

--- src/simple_parse.rs
+++ src/simple_parse.rs
@@ -1,34 +1,30 @@
 /// Extract values from a string with a fixed pattern.
 #[macro_export]
 macro_rules! simple_parse {
-    ($s:expr $(, $parsed:expr)* => _, $suffix:expr $(,)?) => {
+    ($s:expr => $($pattern:tt)+) => {
+        simple_parse!($s, () => $($pattern)+)
+    };
+    ($s:expr, $parsed:expr => _, $suffix:expr $(,)?) => {
         if let Some(_) = $s.strip_suffix($suffix) {
-            Some((
-                $($parsed,)*
-            ))
+            Some($parsed)
         } else {
             None
         }
     };
-    ($s:expr $(, $parsed:expr)* => _, $infix:expr, $($rest:tt)+) => {
+    ($s:expr, $parsed:expr => _, $infix:expr, $($rest:tt)+) => {
         if let Some((_, s)) = $s.split_once($infix) {
-            $crate::simple_parse!(s $(, $parsed)* => $($rest)+)
+            $crate::simple_parse!(s, $parsed => $($rest)+)
         } else {
             None
         }
     };
-    ($s:expr $(, $parsed:expr)* => _ $(,)?) => {
-        Some((
-            $($parsed,)*
-        ))
+    ($s:expr, $parsed:expr => _ $(,)?) => {
+        Some($parsed)
     };
-    ($s:expr $(, $parsed:expr)* => parse $($type:ty)?, $suffix:expr $(,)?) => {
+    ($s:expr, $parsed:expr => parse $($type:ty)?, $suffix:expr $(,)?) => {
         if let Some(val) = $s.strip_suffix($suffix) {
             if let Ok(val) = val.parse$(::<$type>)?() {
-                Some((
-                    $($parsed,)*
-                    val,
-                ))
+                Some($crate::tuple_ops::TupleAppend::append($parsed, val))
             } else {
                 None
             }
@@ -36,10 +32,11 @@ macro_rules! simple_parse {
             None
         }
     };
-    ($s:expr $(, $parsed:expr)* => parse $($type:ty)?, $infix:expr, $($rest:tt)+) => {
+    ($s:expr, $parsed:expr => parse $($type:ty)?, $infix:expr, $($rest:tt)+) => {
         if let Some((val, s)) = $s.split_once($infix) {
             if let Ok(val) = val.parse$(::<$type>)?() {
-                $crate::simple_parse!(s $(, $parsed)*, val => $($rest)+)
+                let parsed = $crate::tuple_ops::TupleAppend::append($parsed, val);
+                $crate::simple_parse!(s, parsed => $($rest)+)
             } else {
                 None
             }
@@ -47,60 +44,48 @@ macro_rules! simple_parse {
             None
         }
     };
-    ($s:expr $(, $parsed:expr)* => parse $($type:ty)? $(,)?) => {
+    ($s:expr, $parsed:expr => parse $($type:ty)? $(,)?) => {
         if let Ok(val) = $s.parse$(::<$type>)?() {
-            Some((
-                $($parsed,)*
-                val,
-            ))
+            Some($crate::tuple_ops::TupleAppend::append($parsed, val))
         } else {
             None
         }
     };
-    ($s:expr $(, $parsed:expr)* => str, $suffix:expr $(,)?) => {
+    ($s:expr, $parsed:expr => str, $suffix:expr $(,)?) => {
         if let Some(val) = $s.strip_suffix($suffix) {
-            Some((
-                $($parsed,)*
-                val,
-            ))
+            Some($crate::tuple_ops::TupleAppend::append($parsed, val))
         } else {
             None
         }
     };
-    ($s:expr $(, $parsed:expr)* => str, $infix:expr, $($rest:tt)+) => {
+    ($s:expr, $parsed:expr => str, $infix:expr, $($rest:tt)+) => {
         if let Some((val, s)) = $s.split_once($infix) {
-            $crate::simple_parse!(s $(, $parsed)*, val => $($rest)+)
+            let parsed = $crate::tuple_ops::TupleAppend::append($parsed, val);
+            $crate::simple_parse!(s, parsed => $($rest)+)
         } else {
             None
         }
     };
-    ($s:expr $(, $parsed:expr)* => str $(,)?) => {
-        Some((
-            $($parsed,)*
-            $s,
-        ))
+    ($s:expr, $parsed:expr => str $(,)?) => {
+        Some($crate::tuple_ops::TupleAppend::append($parsed, $s))
     };
-    ($s:expr $(, $parsed:expr)* => $full:expr $(,)?) => {
+    ($s:expr, $parsed:expr => $full:expr $(,)?) => {
         if $s == $full {
-            Some((
-                $($parsed,)*
-            ))
+            Some($parsed)
         } else {
             None
         }
     };
-    ($s:expr $(, $parsed:expr)* => $prefix:expr $(, $($rest:tt)*)?) => {
+    ($s:expr, $parsed:expr => $prefix:expr $(, $($rest:tt)*)?) => {
         if let Some(s) = $s.strip_prefix($prefix) {
-            $crate::simple_parse!(s $(, $parsed)* => $($($rest)*)?)
+            $crate::simple_parse!(s, $parsed => $($($rest)*)?)
         } else {
             None
         }
     };
-    ($s:expr $(, $parsed:expr)* => $(,)?) => {
+    ($s:expr, $parsed:expr => $(,)?) => {
         if $s.is_empty() {
-            Some((
-                $($parsed,)*
-            ))
+            Some($parsed)
         } else {
             None
         }
